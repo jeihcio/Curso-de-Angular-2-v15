ba0fad96192cd485bdf0629df774e6090b3a95c5
Jeihcio Francis <jeihcio.francis@hotmail.com>
2023-09-04 16:40:26 -0300

    Criando um header compartilhado

--- pokedex/src/app/pages/pages.module.ts
+++ pokedex/src/app/pages/pages.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './routing.module';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    DetailsComponent
-  ],
-  imports: [
-    CommonModule,
-    RoutingModule
-  ]
+    declarations: [
+        HomeComponent,
+        DetailsComponent
+    ],
+    imports: [
+        CommonModule,
+        RoutingModule,
+        SharedModule
+    ]
 })
 export class PagesModule { }
