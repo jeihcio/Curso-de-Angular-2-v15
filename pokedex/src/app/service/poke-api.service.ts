71a999a50946234bb820bf8874b817c70d73ce0a
Jeihcio Francis <jeihcio.francis@hotmail.com>
2023-09-06 16:44:51 -0300

    Encapsulando responsabilidades

--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +17,18 @@ export class PokeApiService {
     return this.http.get<any>(this.url).pipe(
       tap(res => res),
       tap( res => {
-        console.log(res)
+        res.results.map((resPokemon: any) => {
+          this.apiGetPokemons(resPokemon.url).subscribe(
+            res => resPokemon.status = res
+          )
+        });
       })
+    );
+  }
+
+  public apiGetPokemons(url: string): Observable<any>{
+    return this.http.get<any>(url).pipe(
+      map(res => res)
     );
   }
 }
