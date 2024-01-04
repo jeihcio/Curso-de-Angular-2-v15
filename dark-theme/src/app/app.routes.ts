aaa98ea1a1b95b438cedd7548398a8ef7d88eb13
Jeihcio Francis <jeihcio.francis@hotmail.com>
2024-01-04 12:27:19 -0300

    Add os links

--- dark-theme/src/app/app.routes.ts
+++ dark-theme/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './core/pages/components/home/home.component';
+import { SobreComponent } from './core/pages/components/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'sobre',
+        component: SobreComponent
+    }
+];
