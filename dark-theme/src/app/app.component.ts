8952a9543608db36b2dfc608093885e0da8f31ef
Jeihcio Francis <jeihcio.francis@hotmail.com>
2024-01-04 11:49:45 -0300

    Criando o componente de header

--- dark-theme/src/app/app.component.ts
+++ dark-theme/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: '<router-outlet></router-outlet>'
+    selector: 'app-root',
+    standalone: true,
+    template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  `,
+    imports: [CommonModule, RouterOutlet, HeaderComponent]
 })
 export class AppComponent {}
---
+++ dark-theme/src/app/core/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ETheme } from '../../enums/ETheme.enum';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  public icon: string = ETheme.ICON_MOON;
+  public textTheme: string = ETheme.TEXT_MOON;
+
+  public toggle() {
+    const theme = document.body.classList.toggle('dark-theme');
+    
+    if (theme) {
+      this.textTheme = ETheme.TEXT_SUN
+      this.icon = ETheme.ICON_SUN;
+    } else {
+      this.textTheme = ETheme.TEXT_MOON
+      this.icon = ETheme.ICON_MOON;
+    }
+  }
+}
